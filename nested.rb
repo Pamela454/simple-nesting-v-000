41454f0ca4639eabadbfe5a5ae58c896e49b7448
Pamela Frieze <pfrieze@gmail.com>
2017-10-20 08:55:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = [:known_for] << "Ruby"
+programmer_hash[:yukihiro_matsumoto] = [:known_for] 
+		:yukihiro_matsumoto << "Ruby"
 
 end
 
